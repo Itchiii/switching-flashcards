7b3d6256a74124882beb4829ee7ad99bed772a98
Itchiii <raik.loesche@mailbox.org>
2018-04-15 21:07:06 +0200

    spelling mistake and remove unnecessary for-loop
    removed for-loop to check if an animation is running

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 /*jshint esversion: 6 */
 
 /*set own values*/
-const CARD_PEN_OFFSET = 10; //displacement of the cards
-const CARD_SWITCH_RANGE = '130%';
+const CARD_PEN_OFFSET = 10, //displacement of the cards
+      CARD_SWITCH_RANGE = '130%';
 
 
-const CARD_ARRAY = Array.from(document.querySelectorAll('div[class*="card"]'));
+const CARD_ARRAY = [...document.querySelectorAll('div[class*="card"]')];
 /* Do not change this */
 const COUNT_OF_CARDS = CARD_ARRAY.length;
 let last_element = CARD_ARRAY[CARD_ARRAY.length - 1];
 let isMoving = false;
 
 
-let offsetArray = [];
-let offset = 0;
-for (let i = 1; i <= CARD_ARRAY.length; i++) {
+let offsetArray = [], offset = 0, l = CARD_ARRAY.length;
+for (let i = 1; i <= l; i++) {
   offsetArray.push(offset);
   offset += CARD_PEN_OFFSET;
 }
@@ -32,13 +31,12 @@ window.addEventListener('wheel', function(e){cardSwitching(e);});
 window.addEventListener('keydown', function(e){cardSwitching(e);});
 
 function cardSwitching(e) {
-  let animationObject = {};
-  let previousSibling, scrolling = '';
+  let animationObject = {}, previousSibling, scrolling = '';
 
   /* return when you scroll during the animation of a card */
-  for (let index of CARD_ARRAY) {
-    if ((scrolling === 'up') || (scrolling === 'down') || (isMoving)) return;
-  }
+  if ((scrolling === 'up') || (scrolling === 'down') || (isMoving)) return;
+
+  if ((e.keyCode !== 38 && e.keyCode !== 40) && (e.keyCode !== undefined)) return;
 
   for (let index of CARD_ARRAY) {
     if ((parseInt(window.getComputedStyle(index).zIndex) === CARD_ARRAY.length) || (parseInt(index.style.zIndex) === CARD_ARRAY.length)) {
@@ -61,11 +59,11 @@ function cardSwitching(e) {
       if (scrolling === 'down') {
         animationObject.style.zIndex = 0;
         animationObject.style.transform = `translate(${offsetArray[COUNT_OF_CARDS]}px, ${offsetArray[COUNT_OF_CARDS]}px)`;
-        offsetSwtich(scrolling);
+        offsetSwitch(scrolling);
       }
 
       else if (scrolling === 'up'){
-        offsetSwtich(scrolling);
+        offsetSwitch(scrolling);
         animationObject.style.zIndex = COUNT_OF_CARDS;
         animationObject.style.transform = `translate(0px, 0px)`;
       }
@@ -74,7 +72,7 @@ function cardSwitching(e) {
   }
 }
 
-function offsetSwtich(scrolling) {
+function offsetSwitch(scrolling) {
   for (let index of CARD_ARRAY) {
     index.style.zIndex = scrolling === 'down' ? parseInt(index.style.zIndex) + 1 : parseInt(index.style.zIndex) - 1;
     let offsetIndex = Math.abs(parseInt(index.style.zIndex) - COUNT_OF_CARDS);
